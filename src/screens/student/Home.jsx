6dc6f70c2ba589397826635d5b06bbb7e662a192
Anushtha <kumarsen.bash@gmail.com>
2023-09-11 00:23:10 +0530

    home page setup

--- src/screens/student/Home.jsx
+++ src/screens/student/Home.jsx
@@ -1,11 +1,27 @@
-import {View, Text} from 'react-native';
+import {ScrollView, Text, Box, Heading, HStack, Image} from 'native-base';
 import React from 'react';
+
+import hand_img from '../../../assets/images/hand.png';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView px={5} pt={3} bgColor={'white'}>
+      <HStack
+        justifyContent={'space-between'}
+        borderRadius={'md'}
+        padding={4}
+        backgroundColor={'secondary.800'}>
+        <Box flex={0.8}>
+          <Heading color={'white'} bold>
+            Hello,
+          </Heading>
+          <Heading color={'white'} size={'md'} bold>
+            Abhishek!
+          </Heading>
+        </Box>
+        <Image flex={0.2} source={hand_img} alt="hii" size={'md'} />
+      </HStack>
+    </ScrollView>
   );
 };
 
